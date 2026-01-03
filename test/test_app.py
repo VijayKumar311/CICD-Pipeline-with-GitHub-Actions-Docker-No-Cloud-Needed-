535c5d1b66d0eb8c2cbc3ceed9db03569d972c27
VijayKumar311 <138315705+VijayKumar311@users.noreply.github.com>
2026-01-03 15:54:01 +0530

    Delete test directory

--- test/test_app.py
+++
@@ -1,9 +0,0 @@
-import json
-from app.app import app
-
-def test_root():
-    client = app.test_client()
-    resp = client.get("/")
-    data = json.loads(resp.data)
-    assert resp.status_code == 200
-    assert data.get("message") == "Hello from my-app!"
