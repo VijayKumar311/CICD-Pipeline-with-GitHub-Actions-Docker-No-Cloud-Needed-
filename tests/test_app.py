90491f5ea196ec021266a385b7a45cd3f8c95d1d
VijayKumar311 <138315705+VijayKumar311@users.noreply.github.com>
2026-01-06 15:48:32 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,6 +4,5 @@ from app.app import app
 def test_root():
     client = app.test_client()
     resp = client.get("/")
-    data = json.loads(resp.data)
     assert resp.status_code == 200
     assert data.get("message") == "Hello from my-app!"
